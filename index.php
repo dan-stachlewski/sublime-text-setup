be787a8a740146bdc82d61ac08449454c3271cbe
dan-stachlewski <dan.stachlewski@gmail.com>
2015-12-30 13:12:51 +1100

    Added another section.

--- index.php
+++ index.php
@@ -109,6 +109,57 @@
 		
 	</section>
 
+	<section>
+		<h1>LiveReload Issues:</h1>
+		<h2>Installing LiveReload:</h2>
+		<ul>
+			<li>http://anthozano.fr/livereload-pour-sublime-text-3/</li>
+			<li>Access Package Control: Ctrl+Sift+p Type: PCAR</li>
+			<li>As Resource Download: Paste GitHub address: https://raw.github.com/Grafikart/ST3-LiveReload/master/package.json</li>
+			<li>Press Enter</li>
+			<li>Install Package: Via Package Control: Ctrl+Sift+p Type: PCIP</li>
+			<li>Package Control: Ctrl+Sift+p Search: LiveReload <-- Installed</li>
+			<li>Go to: Preferences> Package Settings> LiveReload> Setting - Default</li>
+			<li>Add: {"Enabled_plugins":["SimpleReloadPlugin","SimpleRefresh"]}</li>
+			<li>Add LiveReload Browser Extension: https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei</li>
+			<li>Download and Install LiveReload for Win: http://feedback.livereload.com/knowledgebase/articles/67441-how-do-i-start-using-livereload</li>
+			<li>LiveReload Port Conflicts with Sublime Text 3: https://github.com/toddmotto/fireshell/issues/39</li>
+			<li>Go to: Preferences > Package Settings > Live Reload > Settings - User</li>
+			<li>Type:{ "port" : 35730 }</li>
+			<li>Re Start Sublime <-- No more Port Conflicts</li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<h2>Sublime Text Packages:</h2>
+		<ul>
+			<li>Package Control</li>
+			<li>Emmet</li>
+			<li>Sidebar Enhancements</li>
+			<li>LiveReload</li>
+			<li>Sublime Linter</li>
+			<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>PHP</li>
+			</ul>
+			<li>Sublime Git</li>
+			<li></li>
+			<li>Add: {"Enabled_plugins":["SimpleReloadPlugin","SimpleRefresh"]}</li>
+			<li>Add LiveReload Browser Extension: https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei</li>
+			<li>Download and Install LiveReload for Win: http://feedback.livereload.com/knowledgebase/articles/67441-how-do-i-start-using-livereload</li>
+			<li>LiveReload Port Conflicts with Sublime Text 3: https://github.com/toddmotto/fireshell/issues/39</li>
+			<li>Go to: Preferences > Package Settings > Live Reload > Settings - User</li>
+			<li>Type:{ "port" : 35730 }</li>
+			<li>Re Start Sublime <-- No more Port Conflicts</li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe culpa illo vero adipisci maiores consequuntur! Nobis tempora saepe ut sint distinctio totam laborum quo minima magnam quidem, doloremque, culpa facilis!</p>
+		
+	</section>
 	
 </body>
 </html>
